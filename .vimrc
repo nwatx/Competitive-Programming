e43427e3e13bd520bb51d4cfc4a48b4e98a6569e
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-05-15 13:26:05 -0500

    ubuntu is awesome

---
+++ .vimrc
@@ -0,0 +1,9 @@
+:set ma
+:set buftype=
+:set number
+:set mouse=a
+:let mapleader = ","
+:set relativenumber
+
+inoremap jk <Esc>
+nnoremap ,a gg"+yG
---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int  main() {
+	int n; cin >> n;
+	vector<bool> vis(n+1);
+	for(int i = 0; i < n-1; i++) {
+		int a; cin >> a;
+		vis[a] = true;
+	}
+
+	for(int i = n; i >= 0; i--) {
+		if(!vis[i]) {
+			cout << i; break;
+		}
+	}
+}
