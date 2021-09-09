ba36680a67a0684adeeadc4d6312aa0450ec2d54
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-09-09 16:14:55 -0500

    September 9, 2021 4:14 PM

--- ClosestPair.java
+++ ClosestPair.java
@@ -4,25 +4,25 @@ import java.util.*;
 import static java.lang.System.out;
 
 public class Solution {
-	private static class Pair<T, V> { T l; V r; Pair() {} Pair(T l, V r) { this.l = l; this.r = r; } }
+private static class Pair<T, V> { T l; V r; Pair() {} Pair(T l, V r) { this.l = l; this.r = r; } }
 
-	Pair<double,double> solve(List<Pair<double, double>> v) {
-		Pair<double,Pair<Pair<double, double>,Pair<double, double>>> bes; bes.l = Integer.MAX_VALUE;
-		Set<Pair<double, double>> S; int ind = 0;
-		Collections.sort(v);
-		for(int i = 0; i < v.size(); i++) {
-			if (i && v[i] == v[i-1]) return new Pair(v[i],v[i]);
-			for (; v[i].f-v[ind].f >= bes.f; ++ind) 
-				S.remove(new Pair(v[ind].s,v[ind].f));
-			for (var it = S.ub({v[i].s-bes.f,INF});
-				it != end(S) && it->f < v[i].s+bes.f; ++it) {
-				P t = {it->s,it->f};
-				ckmin(bes,{abs(t-v[i]),{t,v[i]}});
-			}
-			S.insert({v[i].s,v[i].f});
+Pair<double,double> solve(List<Pair<double, double>> v) {
+	Pair<double,Pair<Pair<double, double>,Pair<double, double>>> bes; bes.l = Integer.MAX_VALUE;
+	Set<Pair<double, double>> S; int ind = 0;
+	Collections.sort(v);
+	for(int i = 0; i < v.size(); i++) {
+		if (i && v[i] == v[i-1]) return new Pair(v[i],v[i]);
+		for (; v[i].f-v[ind].f >= bes.f; ++ind) 
+			S.remove(new Pair(v[ind].s,v[ind].f));
+		for (var it = Collections.binarySearch(S, new Pair(v[i].r-bes.l, Integer.MAX_VALUE));
+			it.l < v[i].r+bes.l; ++it) {
+			Pair<double, double> t = new Pair(it.r,it-.l);
+			bes = Math.min(new Pair(Math.abs(t-v[i]),new Pair(t,v[i])));
 		}
-		return bes.s;
+		S.add(new Pair(v[i].r,v[i].l));
 	}
+	return bes.r;
+}
 
 	private static class In {
 		private BufferedReader r;private StringTokenizer tk;
