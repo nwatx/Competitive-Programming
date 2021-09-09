5dafc2bff47ec1398f5b10112ac3b8e3a2906fbf
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-09-09 16:11:42 -0500

    September 9, 2021 4:11 PM

--- ClosestPair.java
+++ ClosestPair.java
@@ -4,17 +4,17 @@ import java.util.*;
 import static java.lang.System.out;
 
 public class Solution {
-	
+	private static class Pair<T, V> { T l; V r; Pair() {} Pair(T l, V r) { this.l = l; this.r = r; } }
 
-	pair<P,P> solve(vP v) {
-		pair<ld,pair<P,P>> bes; bes.f = INF;
-		set<P> S; int ind = 0;
-		sort(all(v));
-		F0R(i,sz(v)) {
-			if (i && v[i] == v[i-1]) return {v[i],v[i]};
+	Pair<double,double> solve(List<Pair<double, double>> v) {
+		Pair<double,Pair<Pair<double, double>,Pair<double, double>>> bes; bes.l = Integer.MAX_VALUE;
+		Set<Pair<double, double>> S; int ind = 0;
+		Collections.sort(v);
+		for(int i = 0; i < v.size(); i++) {
+			if (i && v[i] == v[i-1]) return new Pair(v[i],v[i]);
 			for (; v[i].f-v[ind].f >= bes.f; ++ind) 
-				S.erase({v[ind].s,v[ind].f});
-			for (auto it = S.ub({v[i].s-bes.f,INF});
+				S.remove(new Pair(v[ind].s,v[ind].f));
+			for (var it = S.ub({v[i].s-bes.f,INF});
 				it != end(S) && it->f < v[i].s+bes.f; ++it) {
 				P t = {it->s,it->f};
 				ckmin(bes,{abs(t-v[i]),{t,v[i]}});
