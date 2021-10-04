8b8912982ebe69239d400ce140cd9cfec235a263
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-10-04 13:51:50 -0500

    update problmes

--- contest/codeforces/E_Air_Moped.cpp
+++ contest/codeforces/E_Air_Moped.cpp
@@ -289,73 +289,14 @@ inline namespace FileIO {
 /* #endregion */
 
 /* #region snippets */
-/**
- * Description: 1D point update, range query where \texttt{comb} is
-	 * any associative operation. If $N=2^p$ then \texttt{seg[1]==query(0,N-1)}.
- * Time: O(\log N)
- * Source: 
-	* http://codeforces.com/blog/entry/18051
-	* KACTL
- * Verification: SPOJ Fenwick
- */
 
-// let [l, r] be the distinct values in [l, r]
-// we can query whether something exists in log
-/**
- * Description: range sum queries and point updates for $D$ dimensions
- * Source: https://codeforces.com/blog/entry/64914
- * Verification: SPOJ matsum
- * Usage: \texttt{BIT<int,10,10>} gives 2D BIT
- * Time: O((\log N)^D)
- */
-
-template <class T, int ...Ns> struct BIT {
-	T val = 0; void upd(T v) { val += v; }
-	T query() { return val; }
-};
-template <class T, int N, int... Ns> struct BIT<T, N, Ns...> {
-	BIT<T,Ns...> bit[N+1];
-	template<typename... Args> void upd(int pos, Args... args) { assert(pos > 0);
-		for (; pos<=N; pos+=pos&-pos) bit[pos].upd(args...); }
-	template<typename... Args> T sum(int r, Args... args) {
-		T res=0; for (;r;r-=r&-r) res += bit[r].query(args...); 
-		return res; }
-	template<typename... Args> T query(int l, int r, Args... 
-		args) { return sum(r,args...)-sum(l-1,args...); }
-}; 
-template<class T, int N> int get_kth(const BIT<T,N>& bit, T des) { 
-	assert(des > 0);
-	int ind = 0;
-	for (int i = 1<<bits(N); i; i /= 2)
-		if (ind+i <= N && bit.bit[ind+i].val < des)
-			des -= bit.bit[ind += i].val;
-	assert(ind < N); return ind+1;
-}
 /* #endregion */
 
 const int mx = 2e5+1;
-vi loc[mx];
 
 void solve() {
-	int n, m ; re(n, m);
-	vi v(n);
-	F0R(i, n) {
-		int x; cin >> x;
-		v[i] = x;
-		loc[x].pb(i);
-	}
+	int n; re(n);
 
-	vpi q(m);
-	rep(m) {
-		int1(l, r);
-		q[_] = mp(r, l);
-	}
-
-	sor(q);
-	
-	rep(m) { swap(q[_].f, q[_].s); }
-
-	dbg(q);
 }
 
 signed main() {
